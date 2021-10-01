8531e88500f859b599b642351ae68c98cd8bcbf6
Johannes Seipelt <johannes.seipelt@3m5.de>
2021-10-01 11:38:50 +0200

    add company name change category to module

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,12 +13,13 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Hotspot Wizard',
     'description' => 'This extension provides the hotspot wizard backend module',
-    'category' => 'plugin',
+    'category' => 'module',
     'author' => 'Johannes Seipelt',
     'author_email' => 'johannes.seipelt@3m5.de',
     'state' => 'alpha',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
+    'author_company' => '3m5. Media GmbH',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-10.4.99',
